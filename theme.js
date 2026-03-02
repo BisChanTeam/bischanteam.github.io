daddc39d87b65247a5be95055e37d4ae4980d405
Georgij <goshasoldatov@vk.com>
2026-03-02 16:09:32 +0900

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,6 +1,6 @@
 // MIT License
 
-// Copyright (c) 2025 SupromTeam
+// Copyright (c) 2025-2026 SupromTeam
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
